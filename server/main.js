f0c3784e4d7b53e9808ce23335740fa41881ef37
manupanand <manupanandindianinvestor@gmail.com>
2024-11-07 22:07:17 +0530

    new edit express app

--- server/main.js
+++ server/main.js
@@ -1,1 +1,13 @@
-console.log("express server test")
+const express=require("express")
+const app=express()
+
+
+app.get('/',(req,res)=>{
+    res.json({
+        message:"got main route"
+    })
+})
+
+app.listen('3000',()=>{
+    console.log("app listening to port 3000")
+})
