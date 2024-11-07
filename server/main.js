76edfc3468b3cc6feb27ad2145a375ee11af97d6
manupanand <manupanandindianinvestor@gmail.com>
2024-11-07 23:45:06 +0530

    error for url cors library added

--- server/main.js
+++ server/main.js
@@ -1,13 +1,14 @@
 const express=require("express")
 const app=express()
+const cors=require('cors')
 
-
+app.use(cors())
 app.get('/',(req,res)=>{
     res.json({
         message:"got main route"
     })
 })
 
-app.listen('1500','0.0.0.0',()=>{
-    console.log("app listening to port 1500")
+app.listen('2500','0.0.0.0',()=>{
+    console.log("app listening to port 2500")
 })
